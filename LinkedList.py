009551ad077e2cf3707f8c086791adaaa86e7cca
Alteoziel <99768919+Alteoziel@users.noreply.github.com>
2025-10-04 11:25:10 -0600

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,10 +1,14 @@
 from Node import Node
+import time
+
+
 class LinkedList:
     """A list of (non-sorted) items"""
 
     def __init__(self):
         self.head = None
         self.tail = None
+        self.len = 0
 
     def __str__(self):
         """Returns a string representation of the queue, for example [a, b, c]"""
@@ -22,10 +26,13 @@ class LinkedList:
         temp = Node(item)
         if self.tail == None:
             self.tail = temp
-            self.head = self.tail
+            self.head = temp
+            self.len += 1
             return
         temp.next = self.head
         self.head = temp
+        self.len += 1
+        
 
     def size(self):
         current = self.head
@@ -59,8 +66,12 @@ class LinkedList:
             raise ValueError("{} is not in the list".format(item))
         if previous is None:
             self.head = current.next
+            if current.next == None:
+                self.tail = None
+            self.len -= 1
         else:
             previous.next = current.next
+            self.len -= 1
 
     def append(self, item):
         """This method adds item to the end of the list"""
@@ -68,9 +79,11 @@ class LinkedList:
         if self.tail == None:
             self.tail = tmp
             self.head = self.tail
+            self.len += 1
             return
         self.tail.next = tmp
         self.tail = tmp
+        self.len += 1
 
     def index(self, item):
         """This method returns the position of item in the list or raises a ValueError"""
@@ -93,23 +106,36 @@ class LinkedList:
         current = self.head
         previous = None
         i = 0
-
-        while current is not None:
-            if current.data == item:
+        while self.len >= posn:
+            if i == posn:
                 break
             previous = current
             current = current.next
+            i += 1
 
-        if i == 0:
-            temp = Node(item)
-            self.head = temp
-            self.tail = temp
-            return
         if previous is None:
             temp = Node(item)
-            previous.next = temp
-            temp.next = current
-            
+            if self.tail == None:
+                self.tail = temp
+                self.head = temp
+                self.len += 1
+                return
+            else:
+                a = self.head
+                temp.next = a
+                self.head = temp
+                self.len += 1
+        else:
+            temp = Node(item)
+            if self.tail == None:
+                self.tail = temp
+                self.head = self.tail
+                self.len += 1
+                return
+            else:
+                temp.next = current
+                previous.next = temp
+                self.len += 1
         
 
     def pop(self):
@@ -117,18 +143,39 @@ class LinkedList:
         current = self.head
         previous = None
 
-        while current is not None:
+
+        if current == None:
+            if previous is None:
+                raise ValueError("Nothing in this list.")
+            else:
+                a = previous
+                self.head = None
+                self.tail = None
+                return a.data
+        while current.next is not None:
+            if current.next == None:
+                break
             previous = current
             current = current.next
+            """
+            print(previous)
+            print(current)
+            print(current.next)
+            print("\n")
+            """
 
-        if current is None:
-            raise ValueError("Nothing to pop in the list")
         if previous is None:
-            self.head = current.next
+            a = self.head
+            self.head = None
+            self.tail = None
+            return a.data
+            
         else:
             a = self.tail
-            self.tail = self.previous
-            return a
+            current = None
+            self.tail = previous
+            previous.next = current
+            return a.data
 
     def pop_at_position(self, posn):
         """removes and returns the item at position posn or raises a ValueError."""
@@ -143,6 +190,235 @@ class LinkedList:
             current = current.next
             i+=1
         if previous is None:
+            a = current
             self.head = current.next
+            return a.data
+            
         else:
+            a = current
             previous.next = current.next
+            return a.data
+
+
+
+
+
+
+"""
+
+L = LinkedList()
+l = list()
+
+
+#Test 1
+start = time.time()
+for i in range(1,1001):
+    L.add(1)
+for i in range(1,1001):
+    L.pop_at_position(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,10001):
+    L.add(1)
+for i in range(1,10001):
+    L.pop_at_position(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,100001):
+    L.add(1)
+for i in range(1,100001):
+    L.pop_at_position(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,1000001):
+    L.add(1)
+for i in range(1,1000001):
+    L.pop_at_position(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,1001):
+    l.insert(0,1)
+for i in range(1,1001):
+    l.pop(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,10001):
+    l.insert(0,1)
+for i in range(1,10001):
+    l.pop(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,100001):
+    l.insert(0,1)
+for i in range(1,100001):
+    l.pop(0)
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,1000001):
+    l.insert(0,1)
+for i in range(1,1000001):
+    l.pop(0)
+end = time.time()
+print(end-start)
+
+
+
+
+#Test 2
+start = time.time()
+size = L.size()
+for i in range(1,1001):
+    L.insert(int(size/2), 1)
+for i in range(1,1001):
+    L.pop_at_position(int(size/2))
+end = time.time()
+print(end-start)
+
+size = L.size()
+start = time.time()
+for i in range(1,10001):
+    L.insert(int(size/2), 1)
+for i in range(1,10001):
+    L.pop_at_position(int(size/2))
+end = time.time()
+print(end-start)
+
+size = L.size()
+start = time.time()
+for i in range(1,100001):
+    L.insert(int(size/2), 1)
+for i in range(1,100001):
+    L.pop_at_position(int(size/2))
+end = time.time()
+print(end-start)
+
+size = L.size()
+start = time.time()
+for i in range(1,1000001):
+    L.insert(int(size/2), 1)
+for i in range(1,1000001):
+    L.pop_at_position(int(size/2))
+end = time.time()
+print(end-start)
+
+
+
+size = len(l)
+start = time.time()
+for i in range(1,1001):
+    l.insert(int(size/2),1)
+for i in range(1,1001):
+    l.pop(int(size/2))
+end = time.time()
+print(end-start)
+
+size = len(l)
+start = time.time()
+for i in range(1,10001):
+    l.insert(int(size/2),1)
+for i in range(1,10001):
+    l.pop(int(size/2))
+end = time.time()
+print(end-start)
+
+size = len(l)
+start = time.time()
+for i in range(1,100001):
+    l.insert(int(size/2),1)
+for i in range(1,100001):
+    l.pop(int(size/2))
+end = time.time()
+print(end-start)
+
+size = len(l)
+start = time.time()
+for i in range(1,1000001):
+    l.insert(int(size/2),1)
+for i in range(1,1000001):
+    l.pop(int(size/2))
+end = time.time()
+print(end-start)
+
+
+
+#Test 3
+start = time.time()
+for i in range(1,1001):
+    L.append(1)
+for i in range(1,1001):
+    L.pop()
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,10001):
+    L.append(1)
+for i in range(1,10001):
+    L.pop()
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,100001):
+    L.append(1)
+for i in range(1,100001):
+    L.pop()
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,1000001):
+    L.append(1)
+for i in range(1,1000001):
+    L.pop()
+end = time.time()
+print(end-start)
+
+
+
+start = time.time()
+for i in range(1,1001):
+    l.append(1)
+for i in range(1,1001):
+    l.pop()
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,10001):
+    l.append(1)
+for i in range(1,10001):
+    l.pop()
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,100001):
+    l.append(1)
+for i in range(1,100001):
+    l.pop()
+end = time.time()
+print(end-start)
+
+start = time.time()
+for i in range(1,1000001):
+    l.append(1)
+for i in range(1,1000001):
+    l.pop()
+end = time.time()
+print(end-start)
+"""
