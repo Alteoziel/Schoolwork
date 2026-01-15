3f1c4dd9ed714a5ff7adcff44e3cd6f44b149bdc
Alteoziel <99768919+Alteoziel@users.noreply.github.com>
2026-01-15 14:11:53 -0700

    Update C3 Programming CS 3000.py

--- C3 Programming CS 3000.py
+++ C3 Programming CS 3000.py
@@ -4,7 +4,11 @@ def ran(n):
     a = r.randint(0,10**n)
     b = r.randint(0,10**n)
     return a*b
-    
+
+def ranD(n):
+    a = r.randint(0,10**n)
+    b = r.randint(0,10**n)
+    return a/b
 
 L = []
 n1 = 100000
@@ -12,6 +16,47 @@ n2 = 1000000
 n3 = 10000000 #30
 n4 = 20000000 #78
 n5 = 30000000 #
+
+sT = time.time()
+ranD(n1)
+eT = time.time()
+t = eT-sT
+L.append(t)
+print(t)
+
+sT = time.time()
+ranD(n2)
+eT = time.time()
+t = eT-sT
+L.append(t)
+print(t)
+
+sT = time.time()
+ranD(n3)
+eT = time.time()
+t = eT-sT
+L.append(t)
+print(t)
+
+sT = time.time()
+ranD(n4)
+eT = time.time()
+t = eT-sT
+L.append(t)
+print(t)
+
+sT = time.time()
+ranD(n5)
+eT = time.time()
+t = eT-sT
+L.append(t)
+print(t)
+
+
+
+
+
+
 
 sT = time.time()
 ran(n1)
