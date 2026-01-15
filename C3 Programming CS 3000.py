ea80d3eab28ba56be33e39ed7c406000d32a6b3f
Alteoziel <99768919+Alteoziel@users.noreply.github.com>
2026-01-15 08:58:36 -0700

    Add files via upload

---
+++ C3 Programming CS 3000.py
@@ -0,0 +1,14 @@
+import time, random as r
+
+def ran(n):
+    a = r.randint(0,10**n)
+    b = r.randint(0,10**n)
+    return a*b
+    
+
+for i in range(0,1):
+    sT = time.time()
+    ran(10000000)
+    eT = time.time()
+    t = eT-sT
+    print(t)
