13d3b7eb3b5a4947919bc5ce6fcc05aec2acc649
Alteoziel <99768919+Alteoziel@users.noreply.github.com>
2026-01-22 17:37:34 -0700

    Update C5 programming.py

--- C5 programming.py
+++ C5 programming.py
@@ -25,23 +25,44 @@ def permute_with_all(A):
 
 dictRandom = {}
 a = 0
+l1 = 0
+l2=0
+l3=0
+l4=0
 for j in range(0,1_000_000):
     my_set = [1,2,3,4,5]
     set2=randomize_in_place(my_set)
+    if set2 == [1,2,3,4,5]:
+        l1+=1
+    if set2 == [2,1,3,4,5]:
+        l2+=1
+    if set2 == [3,2,1,4,5]:
+        l3+=1
+    if set2 == [4,2,3,1,5]:
+        l4+=1
     dictRandom[a] = set2
     a+=1
-print(dictRandom[2])
+print("[1,2,3,4,5] = ",l1,"[2,1,3,4,5] = ",l2,"[3,2,1,4,5] = ",l3,"[4,2,3,1,5] = ",l4)
 
-b = (2,1,3,4,5)
-c=0
-for i in range(0,len(dictRandom)):
-    if b in dictRandom:
-        c+=1
 
-print(c)
 
-my_set = [1,2,3,4,5]
-set3=permute_with_all(my_set)
-
-print(set2,set3)
-
+dictRandom = {}
+a = 0
+l1 = 0
+l2=0
+l3=0
+l4=0
+for j in range(0,1_000_000):
+    my_set = [1,2,3,4,5]
+    set2=permute_with_all(my_set)
+    if set2 == [1,2,3,4,5]:
+        l1+=1
+    if set2 == [2,1,3,4,5]:
+        l2+=1
+    if set2 == [3,2,1,4,5]:
+        l3+=1
+    if set2 == [4,2,3,1,5]:
+        l4+=1
+    dictRandom[a] = set2
+    a+=1
+print("[1,2,3,4,5] = ",l1,"[2,1,3,4,5] = ",l2,"[3,2,1,4,5] = ",l3,"[4,2,3,1,5] = ",l4)
