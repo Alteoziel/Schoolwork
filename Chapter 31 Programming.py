6970dddfc1ebcb57ef17384f674108381d70fb36
Aedin Richardson <99768919+Alteoziel@users.noreply.github.com>
2026-02-10 13:58:11 -0700

    Refactor modular_exponentiation and add pseudoprime
    Refactored modular_exponentiation and added pseudoprime function.

--- Chapter 31 Programming.py
+++ Chapter 31 Programming.py
@@ -14,16 +14,24 @@ def extended_euclid(a,b):
         return [d,x,y]
 
 def modular_exponentiation(a,b,n):
-    c = 0
     d = 1
     binary_b = bin(b)[2:]
     print(binary_b)
     for i in range(len(binary_b)-1,0,-1):
-        c = 2*c
         d = (d*d)%n
         if binary_b[i] == 1:
-            c = c + 1
             d = (d*a)%n
     return d
 
+def pseudoprime(n):
+    if modular-exponentiation(2,n-1,n) != 1%n:
+        return "composite"
+    else:
+        return "prime"
+
+
+
+
 print(modular_exponentiation(10,100,3))
+
+
