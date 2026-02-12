d287625524487867f36e239452ede578272b910a
Aedin Richardson <99768919+Alteoziel@users.noreply.github.com>
2026-02-12 09:08:09 -0700

    Refactor pseudoprime and add test prints
    Updated pseudoprime function call and added additional print statements for testing.

--- Chapter 31 Programming.py
+++ Chapter 31 Programming.py
@@ -23,15 +23,16 @@ def modular_exponentiation(a,b,n):
             d = (d*a)%n
     return d
 
+
 def pseudoprime(n):
-    if modular-exponentiation(2,n-1,n) != 1%n:
+    if modular_exponentiation(2,n-1,n) != 1%n:
         return "composite"
     else:
         return "prime"
 
 
 
-
+print(pseudoprime(203025))
 print(modular_exponentiation(10,100,3))
-
-
+print(extended_euclid(1001,2000))
+print(euclid(1001,2000))
